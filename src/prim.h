60264ee2c37bb0518f07d6f093399c9863749177
Tomas <120403622+TB20TB05TB@users.noreply.github.com>
2024-12-16 20:50:28 +0100

    Update prim.h

--- src/prim.h
+++ src/prim.h
@@ -85,9 +85,9 @@ void schreibePrimzahlzwillingeInDatei(const char *dateiName, int von, int bis) {
         return;
     }
     
-    for (int i = von; i <= bis - 2; i++) {
+    for (int i = von; i <= bis - 2; i++) { // -2 weil sonst, wuerde es noch das Intervall bis auf zwei Weitere Stellen pruefen und der Index waere ausserhalb des Intervalls.
         if (istPrimzahl(i) && istPrimzahl(i+2)) {
-            fprintf(datei, "Primzahl=%d DerZwilling=%d\n", i, i+2);
+            fprintf(datei, "Primzahl=%d DerZwilling=%d\n", i, i+2); 
         }
     }
     fclose(datei);
