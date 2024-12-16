ace7681bd66279bdc30af4e06841b7776428c026
Tomas <120403622+TB20TB05TB@users.noreply.github.com>
2024-12-16 20:26:27 +0100

    Update prim.h

--- src/prim.h
+++ src/prim.h
@@ -38,7 +38,7 @@ int primzahlZwilling(int zahl){
     
 }
 
-int schätzePrimBisGauss(float grenze) { //erste Funktion
+int schaetzePrimBisGauss(float grenze) { //erste Funktion
     if (grenze < 2)
     {
         return 0; 
@@ -46,7 +46,7 @@ int schätzePrimBisGauss(float grenze) { //erste Funktion
     return (grenze / log(grenze));
 }
 
-int schätzePrimVonBis(int von, int bis) {
+int schaetzePrimVonBis(int von, int bis) {
 	int differenz = (int) schätzePrimBis(bis) - schätzePrimBis(von);
     return differenz; 
 }
@@ -55,9 +55,25 @@ int schätzePrimVonBis(int von, int bis) {
 int existiertPrimzahlzwillingImIntervall(int von, int bis) {
     
     for (int i = von; i <= bis - 2; i++) { // -2 weil sonst, wuerde es noch das Intervall bis auf zwei Weitere Stellen pruefen und der Index waere ausserhalb des Intervalls.
-        if (istPrimzahl(i) && istPrimzahl(i+2)) {
+        if (istPrimzahl(i) && istPrimzahl(i+2)) { 
             return 1;
         }
     }
     return 0;
+}
+
+void schreibePrimzahlenInDatei(const char *dateiName, int von, int bis) { // ein Zeiger, der auf den Speicherbereich(Datei), wo die Zahlen abgespeichert sind zeigt. Durch const koennen die nicht veraendert werden.  
+    FILE *datei = fopen(dateiName, "w"); 
+	
+    if (!datei) {
+        printf("Fehler beim Öffnen der Datei %s\n", dateiName);
+        return;
+    }
+    
+    for (int i = von; i <= bis; i++) {
+        if (istPrimzahl(i)) {
+            fprintf(datei, "%d\n", i); // fprintf, da wir jede i-te Primzahl in die Datei reinschreiben wollen und nicht in der Konsole ausgeben wollen.
+        }
+    }
+    fclose(datei);
 }
